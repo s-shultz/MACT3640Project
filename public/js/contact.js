f48569d491b30649f1341545fb43e6bf586e0d62
Shaina Shultz <shainashultz@Shainas-MacBook-Pro.local>
2024-05-15 18:02:23 -0700

    added boostrap to contact form

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,17 +1,33 @@
 (function () {
     "use strict";
 
+    let form = document.querySelector('#contact-form-button');
+
     document.querySelector('#contact-form-button').addEventListener('click', (event) => {
-    console.log('Your message was sent!')
+        console.log('Your message was sent!')
 
-    event.preventDefault();
-    event.stopPropagation();
+        event.preventDefault();
+        event.stopPropagation();
 
-    let name = document.querySelector('#name').value;
-    let email = document.querySelector('#mail').value;
-    let message = document.querySelector('#msg').value;
+        let formValid = true;
 
-    console.log("Name: " + name + "\nEmail: " + email + "\nMessage: " + message);
+        if (!form.checkValidity()) {
+            formValid = false;
+        }
+        form.classList.add('was-validated');
+        if (!formValid) {
+            sendTheEmail();
+        }
 
     })
+
+    function sendTheEmail() {
+        let firstName = document.querySelector('#first-name').value;
+        let lastName = document.querySelector('#last-name').value;
+        let email = document.querySelector('#mail').value;
+        let message = document.querySelector('#msg').value;
+
+        console.log("First Name: " + firstName + "\nLast Name: " + lastName + "\nEmail: " + email + "\nMessage: " + message);
+    }
 })();
+
