f89d5a2562c1776003cbbd9441c37aa5213a6e23
Shaina Shultz <shainashultz@Shainas-MacBook-Pro.local>
2024-06-05 12:35:40 -0700

    added database.js

---
+++ utils/database.js
@@ -0,0 +1,31 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+let pool;
+port async function connect() {
+    let cString = "mysql://" + process.env.MYSQL_USER +
+        ":" +
+        process.env.MYSQL_PASSWORD +
+        "@" +
+        process.env.MYSQL_HOST +
+        ":" +
+        process.env.MYSQL_PORT +
+        "/" +
+        process.env.MYSQL_DATABASE;
+    pool = mysql.createPool(
+        {
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE
+        }
+        //cString
+    )
+        .promise();
+}
+
+export async function getAllProjects() {
+    const [rows] = await pool.query('SELECT * FROM projects');
+    return rows;
+}
