4eed6106739074d8cbc569a6ae7545262399bab9
Shaina Shultz <shainashultz@Shainas-MBP.lan>
2024-06-06 14:15:25 -0700

    compared to endofmod06 but issues persist

--- app.js
+++ app.js
@@ -4,21 +4,34 @@ import * as utils from './utils/utils.js';
 
 dotenv.config();
 
+import * as db from './utils/database.js';
+
 let data = ["Project 1", "Project 2", "Project 3"];
+let projects = [];
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res, next) => {
+  await db
+    .connect()
+    .then(async () => {
+      //query the database for projects
+      projects = await db.getAllProjects();
+      console.log(projects);
+      let featuredRand = Math.floor(Math.random() * projects.length);
+      res.render("index.ejs", { featuredProject: projects[featuredRand] });
+    })
+    .catch(next);
+
 });
 
 app.get("/projects", (req, res) => {
-  res.render("projects.ejs", { projectArray: data });
+  res.render("projects.ejs", { data: projects });
 });
 
 app.get("/project/:id", (req, res) => {
@@ -26,7 +39,7 @@ app.get("/project/:id", (req, res) => {
   if (id > data.length) {
     throw new Error("Project does not exist");
   }
-  res.render("project.ejs", { projectArray: data, which: id });
+  res.render("project.ejs", { project: projects[id - 1], which: id });
 });
 
 app.get("/contact", (req, res) => {
@@ -48,10 +61,15 @@ app.post("/mail", async (req, res) => {
 
 app.use(async (err, req, res, next) => {
   console.log(err);
-  res.render("error.ejs");
+  let msg;
+  msg = err.message;
+  if (msg != "No project with that ID") {
+    msg =
+      "There was an internal error. Apologies. We are working on cleaning up the mess.";
+  }
+  res.render("error.ejs", { msg: msg });
 });
 
 app.listen(port, () => {
-  console.log(process.env.SENSITIVE_INFO);
   console.log(`App running @ http://localhost:${port}`);
 })
